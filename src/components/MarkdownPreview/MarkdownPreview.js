955184fe892314dd6449d79e31d302e6e6710644
farukulWD <farukgb1999@gmail.com>
2024-11-07 10:36:29 +0600

    add props type

--- src/components/MarkdownPreview/MarkdownPreview.js
+++ src/components/MarkdownPreview/MarkdownPreview.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Preview from '@uiw/react-markdown-preview';
 
 const source = `
 ## MarkdownPreview
 `;
 
-export default function MarkdownPreview ({text=source}) {
+export default function MarkdownPreview({ text = source }) {
   return (
     <Preview source={text} style={{ padding: 16 }} />
-  )
+  );
 }
+
+// Define prop types
+MarkdownPreview.propTypes = {
+  text: PropTypes.string 
+};
+
+// Define default props
+MarkdownPreview.defaultProps = {
+  text: source
+};
--- src/components/editor/MarkdownEditor.js
+++ src/components/editor/MarkdownEditor.js
@@ -1,42 +1,67 @@
-import { BoldItalicUnderlineToggles, MDXEditor, UndoRedo, headingsPlugin, toolbarPlugin } from '@mdxeditor/editor'
-import React ,{ useEffect, useRef } from 'react';
-import '@mdxeditor/editor/style.css'
+import { BoldItalicUnderlineToggles, MDXEditor, UndoRedo, headingsPlugin, toolbarPlugin } from '@mdxeditor/editor';
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types'
+import '@mdxeditor/editor/style.css';
 
 function MarkdownEditor({
-    value='# Hello World',
+    value = '# Hello World',
     onChange,
     toolbarProps,
     placeholder = "Write Here...",
-    items = []}) {
-        const editorRef = useRef(null);
-        useEffect(() => {
-            if (editorRef.current) {
-              const currentMarkdown = editorRef.current.getMarkdown();
-              if (currentMarkdown !== value) {
+    items = []
+}) {
+    const editorRef = useRef(null);
+
+    useEffect(() => {
+        if (editorRef.current) {
+            const currentMarkdown = editorRef.current.getMarkdown();
+            if (currentMarkdown !== value) {
                 editorRef.current.setMarkdown(value);
-              }
             }
-          }, [value]);
+        }
+    }, [value]);
 
+    const handleEditorChange = () => {
+        const markdownContent = editorRef?.current?.getMarkdown() || "";
+        onChange(markdownContent);
+    };
 
-        const handleEditorChange = () => {
-            const markdownContent = editorRef?.current?.getMarkdown() || "";
-        onChange(markdownContent)}
-
-  return <MDXEditor       onChange={handleEditorChange}   ref={editorRef}  markdown={value} placeholder={placeholder} plugins={[
-    headingsPlugin(),
-    toolbarPlugin({
-      toolbarClassName: 'my-classname',
-    
-      toolbarContents: () => (
-        <>
-          {' '}
-          <UndoRedo />
-          <BoldItalicUnderlineToggles />
-        </>
-      )
-    })
-  ]} />
+    return (
+        <MDXEditor
+            onChange={handleEditorChange}
+            ref={editorRef}
+            markdown={value}
+            placeholder={placeholder}
+            plugins={[
+                headingsPlugin(),
+                toolbarPlugin({
+                    toolbarClassName: 'my-classname',
+                    toolbarContents: () => (
+                        <>
+                            <UndoRedo />
+                            <BoldItalicUnderlineToggles />
+                        </>
+                    )
+                })
+            ]}
+        />
+    );
 }
 
-export default MarkdownEditor
+// Define prop types
+MarkdownEditor.propTypes = {
+    value: propTypes.string,
+    onChange: PropTypes.func.isRequired,
+    toolbarProps: PropTypes.object,
+    placeholder: PropTypes.string,
+    items: PropTypes.array
+};
+
+// Define default props
+MarkdownEditor.defaultProps = {
+    value: '# Hello World',
+    placeholder: 'Write Here...',
+    items: []
+};
+
+export default MarkdownEditor;
